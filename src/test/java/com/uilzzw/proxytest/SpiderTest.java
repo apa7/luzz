dd7bad401961904aacde2ee26302922fc0ae3cb1
uilzzw <uilzzw@gmail.com>
2016-05-25 22:57:14 +0800

    test

--- src/test/java/com/uilzzw/proxytest/SpiderTest.java
+++ src/test/java/com/uilzzw/proxytest/SpiderTest.java
@@ -6,7 +6,7 @@ import com.uilzzw.common.ConstantUtils;
 import com.uilzzw.common.UserAgent;
 import com.uilzzw.proxy.OutputOrInputProxy;
 import com.uilzzw.proxy.SpiderProxy;
-import com.uilzzw.utils.ProxyConstants;
+import com.uilzzw.utils.ProxyWebSite;
 
 
 public class SpiderTest {
@@ -14,10 +14,10 @@ public class SpiderTest {
 	public static void main(String[] args) {
 		String userAgent = UserAgent.GOOGLECHROME_WINDOWS;
 		for (int i = 1; i <= 10; i++) {
-			String url = ProxyConstants.NT_PROXY + i;
+			String url = ProxyWebSite.NT_PROXY + i;
 			List<HashMap<String, String>> xiciProxy = SpiderProxy.getXiciProxy(url, userAgent);
 			ConstantUtils.getLogger().info(xiciProxy.toString());
-			OutputOrInputProxy.writeContentToFile(xiciProxy, null, true, true, false);
+			OutputOrInputProxy.writeContentToFile(xiciProxy, null, true, true, true);
 		}
 	}
 }
