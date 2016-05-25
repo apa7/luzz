e29c36024072989bed09303d7801f09571bd6d03
uilzzw <uilzzw@gmail.com>
2016-05-25 22:08:39 +0800

    .

--- src/main/java/com/uilzzw/beans/ProxyEntity.java
+++ src/main/java/com/uilzzw/beans/ProxyEntity.java
@@ -1,43 +1,42 @@
 package com.uilzzw.beans;
 
 public class ProxyEntity {
-    private String ipAddress;
-    private String port;
-    private String protocol;
-    // 0-Can used;1-Can't used;2-Don't Validate;3-Deleted;4-Died;5-Alived;
-    private int status;
+	private String ipAddress;
+	private int port;
+	private String protocol;
+	// 0-Can used;1-Can't used;2-Don't Validate;3-Deleted;4-Died;5-Alived;
+	private int status;
 
-    public String getIpAddress() {
-	return ipAddress;
-    }
+	public String getIpAddress() {
+		return ipAddress;
+	}
 
-    public void setIpAddress(String ipAddress) {
-	this.ipAddress = ipAddress;
-    }
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
 
-    public String getPort() {
-	return port;
-    }
+	public int getPort() {
+		return port;
+	}
 
-    public void setPort(String port) {
-	this.port = port;
-    }
+	public void setPort(int port) {
+		this.port = port;
+	}
 
-    public String getProtocol() {
-	return protocol;
-    }
+	public String getProtocol() {
+		return protocol;
+	}
 
-    public void setProtocol(String protocol) {
-	this.protocol = protocol;
-    }
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
 
-    public int getStatus() {
-        return status;
-    }
+	public int getStatus() {
+		return status;
+	}
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
+	public void setStatus(int status) {
+		this.status = status;
+	}
 
-   
 }
