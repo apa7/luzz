893e39d80fd9216b2ac77f2b2f35e7885f4fa1e5
uilzzw <uilzzw@gmail.com>
2016-05-25 21:50:38 +0800

    change class name

--- src/main/java/com/uilzzw/utils/ProxyWebSite.java
+++ src/main/java/com/uilzzw/utils/ProxyWebSite.java
@@ -1,12 +1,11 @@
 package com.uilzzw.utils;
 
-public class ProxyConstants {
-
-    // 默认保存的文件名
-    public static final String FILE_NAME = "proxy.txt";
+public class ProxyWebSite {
 
     /**
+     * 
      * 西刺代理 NN-->国内高度匿名; WN-->国外高度匿名; NT-->国内透明代理; WT-->国内透明代理; SOCKS-->socks代理;
+     * 
      */
     public static final String NN_PROXY = "http://www.xicidaili.com/nn/";
     public static final String WN_PROXY = "http://www.xicidaili.com/wn/";
