0faa29e4513e5cc657f9b3b165ecdbb3d82acd5c
uilzzw <uilzzw@gmail.com>
2016-05-27 23:43:43 +0800

    change cachedthreadpool

--- src/main/java/com/uilzzw/proxy/ValidationTask.java
+++ src/main/java/com/uilzzw/proxy/ValidationTask.java
@@ -4,13 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
 
 import com.uilzzw.beans.ProxyEntity;
 import com.uilzzw.common.ConstantUtils;
 
 public class ValidationTask {
+	private ExecutorService threadPool;
+	
+	
+	public ExecutorService getThreadPool() {
+		return threadPool;
+	}
+
+	public void setThreadPool(ExecutorService threadPool) {
+		this.threadPool = threadPool;
+	}
+
+	public ValidationTask() {
+		super();
+	}
+
+	public ValidationTask(ExecutorService threadPool) {
+		super();
+		this.threadPool = threadPool;
+	}
+
 	/**
 	 * 
 	 * @param list
@@ -19,7 +39,7 @@ public class ValidationTask {
 	 *            线程池
 	 * @return
 	 */
-	public List<ProxyEntity> getCanUsedProxy(List<Map<String, String>> list, ScheduledExecutorService threadPool) {
+	public List<ProxyEntity> getCanUsedProxy(List<Map<String, String>> list, ExecutorService threadPool) {
 		if (list == null || list.size() == 0 || threadPool == null)
 			return null;
 		List<Future<ProxyEntity>> proxyList = new ArrayList<Future<ProxyEntity>>();
@@ -45,8 +65,45 @@ public class ValidationTask {
 				e.printStackTrace();
 			}
 		}
-		threadPool.shutdown();
+		//threadPool.shutdown();
 		return canUsedList;
 	}
-
+	
+	/**
+	 * 
+	 * @param list
+	 *            从文件读取到的代理list
+	 * @param threadPool
+	 *            线程池
+	 * @return
+	 */
+	public List<ProxyEntity> getCanUsedProxy(List<Map<String, String>> list) {
+		if (list == null || list.size() == 0 || threadPool == null)
+			return null;
+		List<Future<ProxyEntity>> proxyList = new ArrayList<Future<ProxyEntity>>();
+		for (Map<String, String> map : list) {
+			String ipAddr = map.get("ipAddr");
+			String port = map.get("port");
+			String protocol = map.get("protocol");
+			ValidateProxy task = new ValidateProxy(ipAddr, Integer.parseInt(port), protocol,
+					ConstantUtils.VALIDATION_SITE, ConstantUtils.ASSERT);
+			Future<ProxyEntity> future = threadPool.submit(task);
+			proxyList.add(future);
+		}
+		List<ProxyEntity> canUsedList = new ArrayList<ProxyEntity>();
+		for (Future<ProxyEntity> future : proxyList) {
+			try {
+				ProxyEntity entity = future.get();
+				if (entity.getStatus() == 1)
+					continue;
+				canUsedList.add(entity);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+		//threadPool.shutdown();
+		return canUsedList;
+	}
 }
