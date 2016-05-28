60050d6ea386a86c55b86df8934124d1ce7ae1f5
uilzzw <uilzzw@gmail.com>
2016-05-28 16:57:20 +0800

    ...

--- src/test/java/com/uilzzw/proxytest/ReadAndValidate.java
+++ src/test/java/com/uilzzw/proxytest/ReadAndValidate.java
@@ -13,16 +13,14 @@ public class ReadAndValidate {
 	public static void main(String[] args) {
 		String filePath = ConstantUtils.getUserHome() + "\\" + ConstantUtils.FILE_NAME;
 		List<Map<String, String>> list = OutputOrInputProxy.readProxyFromFile(filePath);
-		// new a threadPool
 		List<List<Map<String,String>>> split = CommonUtils.split(list, 125);
 
 		System.out.println(split.size());
-		
-		
-		//ExecutorService threadPool = CommonUtils.createThreadPool("ValidationPool");
+		// new a threadPool
+		ExecutorService threadPool = CommonUtils.createThreadPool("ValidationPool"); 
 		for (int i = 0; i < split.size(); i++) {
-			//List<ProxyEntity> list2 = new ValidationTask().getCanUsedProxy(split.get(i),threadPool);
-			List<ProxyEntity> list2 = new ValidationTask(CommonUtils.createThreadPool("ValidationPool")).getCanUsedProxy(split.get(i));
+			List<ProxyEntity> list2 = new ValidationTask().getCanUsedProxy(split.get(i),threadPool);
+			//List<ProxyEntity> list2 = new ValidationTask(CommonUtils.createThreadPool("ValidationPool")).getCanUsedProxy(split.get(i));
 			System.out.println(list2);
 		}
 	}
